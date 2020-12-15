b5cc62ac68e52f6a569b0ab37422afcbea14e6d4
Courtney Carson <cmacarson@gmail.com>
2020-12-15 14:57:26 -0800

    passing cypress tests

--- src/components/Learning/Learning.js
+++ src/components/Learning/Learning.js
@@ -3,7 +3,7 @@ import UserContext from '../../contexts/UserContext';
 import config from '../../config';
 import TokenService from '../../services/token-service';
 import { Input, Label } from '../Form/Form';
-import Button from '../Button/Button'
+import Button from '../Button/Button';
 
 
 class Learning extends Component {
@@ -51,25 +51,27 @@ class Learning extends Component {
     //display current total score -- this.context.totalScore
     //display form with input input#learn-guess-input 
     //display correct & incorrect count for word -- this.context.correctCount? wordCorrectCount - from response
-   //button 
+    //button 
     console.log(this.context.nextWord);
     return (
       <div className='learning'>
-        <h2>Translate {this.context.nextWord ? this.context.nextWord.nextWord:null}</h2>
-        <p> you've answered correctly: {this.context.nextWord ? this.context.nextWord.wordCorrectCount : null}</p>
-        <p>you've answered incorrectly {this.context.nextWord ? this.context.nextWord.wordIncorrectCount : null}</p>
-        <p>total score {this.context.nextWord ? this.context.nextWord.totalScore : null}</p>
-        <form>
-          <Label htmlFor='learning-form' className='learning-form-text'>
-            Guess
+        <main>
+          <form>
+            <Label htmlFor='learn-guess-input' className='learning-form-text'>
+              What's the translation for this word?
           </Label>
-          <Input className='text-box'
-            id='guess-box'
-            required
-          />
-          <Button >Next</Button>
+            <Input className='text-box'
+              id='learn-guess-input'
+              required
+            />
+            <Button type='submit'>Submit your answer</Button>
+          </form>
+          <h2>Translate the word:</h2><span>{this.context.nextWord ? this.context.nextWord.nextWord : null}</span>
+          <p>Your total score is: {this.context.nextWord ? this.context.nextWord.totalScore : null}</p>
+          <p>You have answered this word correctly {this.context.nextWord ? this.context.nextWord.wordCorrectCount : null} times.</p>
+          <p>You have answered this word incorrectly {this.context.nextWord ? this.context.nextWord.wordIncorrectCount : null} times.</p>
 
-        </form>
+        </main>
       </div>
     );
   }
